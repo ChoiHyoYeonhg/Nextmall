129daf72f4c99a2314982a06daba415ff823458c
Choi <chy71068@naver.com>
2022-10-13 12:18:36 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Layout from '../components/Layout'
+import data from '../utils/data'
+import ProductItem from '../components/ProductItem'
+
+export default function Home() {
+  return <Layout title='Home Page'>
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4
+    xl:grid-cols-5 2xl:grid-cols-6">
+      {data.products.map((product) => (
+        <ProductItem product={product} key={product.slug}></ProductItem>
+      ))}
+    </div>
+  </Layout>
+}
