20aa7a7f39d03cc629365e8dde726ab5bd02ae8e
Choi <chy71068@naver.com>
2022-10-19 20:51:34 +0900

    first

--- components/Layout.js
+++ components/Layout.js
@@ -1,49 +1,52 @@
-import Head from "next/head"
-import Link from "next/link"
-import { useContext } from 'react'
-import { Store } from '../utils/Store'
+import Head from "next/head";
+import Link from "next/link";
+import { useContext } from "react";
+import { Store } from "../utils/Store";
 
-export default function Layout({title, children}) {
-    const { state } = useContext(Store)
-    const { cart } = state
+export default function Layout({ title, children }) {
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
     <>
-    <Head>
-        <title>{title ? title + 'NextShop' : 'NextShop'} </title>
-        <meta name='description' content='Nextjs Ecommerce' />
-        <link rel='icon' href='/favocon.ico' />
-    </Head>
+      <Head>
+        <title>{title ? title + "NextShop" : "NextShop"} </title>
+        <meta name="description" content="Nextjs Ecommerce" />
+        <link rel="icon" href="/favocon.ico" />
+      </Head>
 
-    <div className="flex min-h-screen flex-col justify-between">
-        <header> 
-            <nav className="flex h-12 items-center px-4 justify-between shadow-md bg-slate-200">
-                <Link href="/">
-                    <a className="text-lg font-bold text-red-500">NextShop</a>
-                </Link>
-                <div>
-                    <Link href="/cart">
-                        <a className="/p-2 text-black">
-                            Cart 
-                            {cart.cartItems.length > 0 && (
-                                <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
-                                    {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
-                                </span>
-                            )}
-                            </a>
-                    </Link>
-                    <Link href="/login">
-                        <a className="/p-2 text-black"> Login </a>
-                    </Link>
-                </div>
-            </nav>
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex h-12 items-center px-4 justify-between shadow-md bg-slate-200">
+            <Link href="/">
+              <a className="text-lg font-bold text-red-500">NextShop</a>
+            </Link>
+            <div>
+              <Link href="/contect">
+                <a className="p-2 text-black">소개</a>
+              </Link>
+              <Link href="/cart">
+                <a className="/p-2 text-black">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    </span>
+                  )}
+                </a>
+              </Link>
+              <Link href="/login">
+                <a className="/p-2 text-black"> Login </a>
+              </Link>
+            </div>
+          </nav>
         </header>
 
         <main className="container m-auto mt-4 px-4"> {children} </main>
 
         <footer className="flex h-10 justify-center items-center shadow-inner bg-red-100">
-            <p>Copyright &copy; 2022 NextShop</p>
+          <p>Copyright &copy; 2022 NextShop</p>
         </footer>
-    </div>
+      </div>
     </>
-  )
+  );
 }
